2b11b0c66d0966876483f6f2b6ce76050b5d9a9f
elanonimo <mgrbmk@emailhosting.com>
2018-08-29 14:19:21 +0300

    fixed react devtools extension

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,7 +4,7 @@ import filtersReducer from '../reducers/filters';
 import authReducer from '../reducers/auth';
 import thunk from 'redux-thunk';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_CCOMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
   const store = createStore(
